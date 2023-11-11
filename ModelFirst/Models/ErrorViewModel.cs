a61d7368d3454b7893f3dc037ecef8161411b232
jgarcia60436 <78770752+jgarcia60436@users.noreply.github.com>
2023-11-10 18:44:47 -0600

    Agregar archivos de proyecto.

---
+++ ModelFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModelFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
